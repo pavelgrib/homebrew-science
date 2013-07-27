a319f94b481ef23542612936d6e9f79bed86dcc3
pavelgrib <paul.quant@gmail.com>
2013-07-27 02:06:13 +0100

    updated petsc from 3.3.5-p5 to 3.4.2

--- petsc.rb
+++ petsc.rb
@@ -2,8 +2,9 @@ require 'formula'
 
 class Petsc < Formula
   homepage 'http://www.mcs.anl.gov/petsc/index.html'
-  url 'http://ftp.mcs.anl.gov/pub/petsc/release-snapshots/petsc-3.3-p5.tar.gz'
-  sha1 'ac8e64b1b266c3b8779a6dd91f21e09cf3442321'
+  url 'http://ftp.mcs.anl.gov/pub/petsc/release-snapshots/petsc-3.4.2.tar.gz'
+  sha1 '9ba201c73ca85e01da37176b7bbf80a75756c515'
+  #sha1 'ac8e64b1b266c3b8779a6dd91f21e09cf3442321'
 
   depends_on :mpi => :cc
   depends_on :fortran
